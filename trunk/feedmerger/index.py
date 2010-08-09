6fe23e9084f5091d711aa51f285e76e2d256172a
pedromcaraujo@gmail.com <pedromcaraujo@gmail.com@e90ad796-6b4a-e173-e2bf-e95fb328c886>
2010-08-09 16:04:58 +0000

    Front page template, login, editing feed list.

--- trunk/feedmerger/index.py
+++ trunk/feedmerger/index.py
@@ -1,13 +1,43 @@
 from google.appengine.ext import webapp
+from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from data import Feed
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        user = users.get_current_user()
+        logged_in = False
+        template_values = {}
+
+        if user:
+            key = self.request.get('remove')
+            if key:
+                Feed.get(key).delete()
+            logged_in = True
+            url = users.create_logout_url(self.request.uri)
+            feeds = Feed.all().filter('owner =',user)
+            template_values.update({'feeds':feeds})
+        else:
+            logged_in = False
+            url = users.create_login_url(self.request.uri)
+
+        template_values.update({'logged':logged_in, 'login_url':url})
+
+        self.response.out.write(template.render('index.pt',template_values))
+
+    def post(self):
+        user = users.get_current_user()
+        feed = self.request.get('feed')
+        # check if feed is valid
+        feed_obj = Feed(owner=user,feed=feed)
+        feed_obj.put()
+        self.redirect('/')
 
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [
+                                         ('/', MainPage),
+                                     ],
                                      debug=True)
 
 def main():
