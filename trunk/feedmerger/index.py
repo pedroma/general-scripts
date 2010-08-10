4e72db3c682d30053f7e30d3eefb75f83b39bfbb
pedromcaraujo@gmail.com <pedromcaraujo@gmail.com@e90ad796-6b4a-e173-e2bf-e95fb328c886>
2010-08-10 15:15:59 +0000

    First final version. With ordered feeds and asynchronous requests.

--- trunk/feedmerger/index.py
+++ trunk/feedmerger/index.py
@@ -3,7 +3,7 @@ from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from data import Feed
-import feedparser
+import feedparser, os
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -15,16 +15,22 @@ class MainPage(webapp.RequestHandler):
             if key:
                 try:
                     Feed.get(key).delete()
+                    self.redirect('/')
                 except:
                     pass
+            if key == 'all':
+                feeds = Feed.all().filter('owner =',user)
+                for feed in feeds:
+                    feed.delete()
+                    self.redirect('/')
             logged_in = True
             url = users.create_logout_url(self.request.uri)
             feeds = Feed.all().filter('owner =',user)
             n_feeds = feeds.count()
             if n_feeds != 0:
-                feed_url = 'http://feedmerger.appspot.com/feed/%s'%feeds[0].key()
+                feed_url = 'http://%s/feed/%s'%(os.environ['HTTP_HOST'],feeds[0].key())
                 template_values.update({'feed_url':feed_url})
-            template_values.update({'feeds':feeds,'n_feeds':n_feeds})
+            template_values.update({'feeds':feeds,'n_feeds':n_feeds,'nick':user})
         else:
             logged_in = False
             url = users.create_login_url(self.request.uri)
@@ -40,6 +46,7 @@ class MainPage(webapp.RequestHandler):
         lines = feed.split('\n')
         for line in lines:
             f = feedparser.parse(line)
+            line = line.replace('\r','')
             if f.get('status',0) in [200,302]:
                 feed_obj = Feed(title=f.feed.title,owner=user,feed=line)
                 feed_obj.put()
