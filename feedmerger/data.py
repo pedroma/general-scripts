4021759c300cef4e85d213279297942ad475f7c4
pedromcaraujo@gmail.com <pedromcaraujo@gmail.com@e90ad796-6b4a-e173-e2bf-e95fb328c886>
2010-08-11 15:21:17 +0000

    Created a repository to host feedmerger

--- feedmerger/data.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-class Feed(db.Model):
-    title = db.StringProperty()
-    owner = db.UserProperty()
-    feed = db.URLProperty(required=True)
-    limit = db.IntegerProperty(default = 10)
